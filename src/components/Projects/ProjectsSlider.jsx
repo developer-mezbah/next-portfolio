5ffdb6e49d7141800b66c319a1d6bc38a5528bb1
developer.mezbah <developer.mezbah@gmail.com>
2024-02-23 15:29:11 +0600

    services slider

--- src/components/Projects/ProjectsSlider.jsx
+++ src/components/Projects/ProjectsSlider.jsx
@@ -8,61 +8,64 @@ import "swiper/css/pagination";
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
-const ProjectsSlider = ({data}) => {
+const ProjectsSlider = ({ data }) => {
   return (
-      <Swiper
-        cssMode={true}
-        loop={true}
-        autoplay={{
-          delay: 3000,
-          disableOnInteraction: false,
-          pauseOnMouseEnter: true,
-        }}
-        navigation={{
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Navigation, Pagination, Autoplay]}
-        className="project_slider md:h-[310px]"
-      >
-        {data?.map((slide) => (
-          <SwiperSlide key={slide.id} className="px-16 pb-10"><Link
-          href={"/project-details"}>
-            <div className="md:flex justify-between gap-6 items-center mx-auto block w-full text-center md:text-left">
-              <div className="md:w-[90%] w-full">
-                <Image
-                  className="rounded-lg sm:h-[220px] md:w-full mx-auto mb-5"
-                  width={400}
-                  height={280}
-                  src={slide.img}
-                  alt={slide.title}
-                />
-              </div>
-              <div className="w-full md:space-y-5">
-                <h2 className="portfolio__title">{slide.title}</h2>
-                <p className="portfolio__description">{slide.description}</p>
-                <button
-                  className="button button--flex button--small portfolio__button cursor-pointer"
-                >
+    <Swiper
+      cssMode={true}
+      loop={true}
+      autoplay={{
+        delay: 3000,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
+      }}
+      navigation={{
+        nextEl: ".swiper-button-prev",
+        prevEl: ".swiper-button-next",
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Navigation, Pagination]}
+      className="project_slider md:h-[310px]"
+    >
+      {data?.map((slide) => (
+        <SwiperSlide
+          key={slide.id}
+          className="md:px-16 pb-10"
+          style={{ background: "var(--hue-color)" }}
+        >
+          <div className="md:flex justify-between gap-6 items-center mx-auto block w-full text-center md:text-left">
+            <div className="md:w-[90%] w-full">
+              <Image
+                className="rounded-lg sm:h-[220px] md:w-full mx-auto mb-5"
+                width={400}
+                height={280}
+                src={slide.img}
+                alt={slide.title}
+              />
+            </div>
+            <div className="w-full md:space-y-5">
+              <h2 className="portfolio__title">{slide.title}</h2>
+              <p className="portfolio__description">{slide.description}</p>
+              <Link href={"/project-details"}>
+                <button className="button button--flex button--small portfolio__button cursor-pointer my-3">
                   preview
                   <VscSend className="button__icon" />
                 </button>
-              </div>
-            </div></Link>
-          </SwiperSlide>
-        ))}
-        <div className="absolute w-full -top-[40px] h-full z-50 flex justify-between items-center">
-          <div className="swiper-button-next cursor-pointer overflow-hidden text-sm">
-            <IoIosArrowBack className="swiper-portfolio-icon" />
+              </Link>
+            </div>
           </div>
-          <div className="swiper-button-prev cursor-pointer">
-            <IoIosArrowForward className="swiper-portfolio-icon" />
-          </div>
+        </SwiperSlide>
+      ))}
+      <div className="absolute w-full top-[100px] z-50 flex justify-between items-center">
+        <div className="swiper-button-next cursor-pointer overflow-hidden text-sm">
+          <IoIosArrowBack className="swiper-portfolio-icon" />
         </div>
-      </Swiper>
+        <div className="swiper-button-prev cursor-pointer">
+          <IoIosArrowForward className="swiper-portfolio-icon" />
+        </div>
+      </div>
+    </Swiper>
   );
 };
 
