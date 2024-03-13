33cc174fbabd7705d2bf74d6648b25316eafc589
developer.mezbah <developer.mezbah@gmail.com>
2024-03-13 19:59:59 +0600

    blog details added

---
+++ src/app/( Frontend )/blogs/[id]/page.jsx
@@ -0,0 +1,12 @@
+import BlogDetails from "@/components/Blogs/BlogDetails";
+import MasterLayout from "@/layout/MasterLayout";
+
+const SingleBlog = () => {
+  return (
+    <MasterLayout>
+      <BlogDetails />
+    </MasterLayout>
+  );
+};
+
+export default SingleBlog;
