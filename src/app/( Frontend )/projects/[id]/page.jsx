a8a17433ac48de2942ca6ee701690dfd69d48eb8
developer.mezbah <developer.mezbah@gmail.com>
2024-04-30 19:01:05 +0600

    testing-2

--- src/app/( Frontend )/projects/[id]/page.jsx
+++ src/app/( Frontend )/projects/[id]/page.jsx
@@ -2,26 +2,32 @@ import ProjectDetails from "@/components/ProjectDetails/ProjectDetails";
 import MasterLayout from "@/layout/MasterLayout";
 import { PrismaClient } from "@prisma/client";
 
-async function getData(id){
+async function getData(id) {
   const prisma = new PrismaClient();
   const projects = await prisma.projects.findUnique({
-    where: {id},
-    include: {profile: {select: {user_name: true, img: true}}, key_feature: true, for_developer: true}
-  })
+    where: { id },
+    include: {
+      profile: { select: { user_name: true, img: true } },
+      key_feature: true,
+      for_developer: true,
+    },
+  });
   const relatedProjects = await prisma.projects.findMany({
-    where: {categoryId: projects.categoryId}
-  })
-  return {projects, relatedProjects}
+    where: { categoryId: projects.categoryId },
+  });
+  return { projects, relatedProjects };
 }
 
-
 const ProjectsDetails = async (props) => {
-    let id = await parseInt(props.searchParams["id"]);
-    const data = await getData(id);
+  let id = await parseInt(props.searchParams["id"]);
+  const data = await getData(id);
   return (
     <MasterLayout>
       <div>
-        <ProjectDetails data={data?.projects} relatedProjects={data?.relatedProjects}/>
+        <ProjectDetails
+          data={data?.projects}
+          relatedProjects={data?.relatedProjects}
+        />
       </div>
     </MasterLayout>
   );
