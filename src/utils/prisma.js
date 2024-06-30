ae5f2fd33d221fdda2abc12697a284be8decf267
developer-mezbah <developer.mezbah@gmail.com>
2024-06-30 20:11:02 +0600

    prisma client upadate

--- src/utils/prisma.js
+++ src/utils/prisma.js
@@ -1,7 +1,18 @@
 import { PrismaClient } from "@prisma/client"
 
 const prismaClientSingleton = () => {
-  return new PrismaClient()
+  return new PrismaClient().$extends({
+    result: {
+      user: {
+        fullName: {
+          needs: { firstName: true, lastName: true },
+          compute(user) {
+            return `${user.firstName} ${user.lastName}`
+          },
+        },
+      },
+    },
+  })
 }
 
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
