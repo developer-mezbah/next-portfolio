f58724e74c2e990042a57a7005838ae928ec1137
developer-mezbah <developer.mezbah@gmail.com>
2024-06-30 15:32:26 +0600

    layout update

--- src/layout/MasterLayout.jsx
+++ src/layout/MasterLayout.jsx
@@ -1,24 +1,26 @@
+export const revalidate = 0;
 import React from "react";
 import Navbar from "./Header/Navbar";
 import Footer from "./Footer/Footer";
 import { PrismaClient } from "@prisma/client";
 
 async function getData() {
-  const prisma = new PrismaClient()
+  const prisma = new PrismaClient();
   const social = await prisma.Social_media.findFirst({});
   const web_info = await prisma.Web_information.findFirst({});
 
-  return {social,web_info}
+  return { social, web_info };
 }
 
-
 const MasterLayout = async ({ children }) => {
-  const data = await getData()
+  const data = await getData();
   return (
     <div>
       <Navbar data={data?.web_info} />
-      <div className="main" style={{marginTop: "100px"}}>{children}</div>
-      <Footer social={data?.social} data={data?.web_info}/>
+      <div className="main" style={{ marginTop: "100px" }}>
+        {children}
+      </div>
+      <Footer social={data?.social} data={data?.web_info} />
     </div>
   );
 };
