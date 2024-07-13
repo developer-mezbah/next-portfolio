c788c5a7de329dc2c2ba8261f18c942381f8ec4f
developer-mezbah <developer.mezbah@gmail.com>
2024-07-13 11:41:03 +0600

    update for seo

--- src/app/layout.js
+++ src/app/layout.js
@@ -81,19 +81,19 @@ export const metadata = {
     address: "Dhaka, Bangladesh",
     telephone: "+880 1843504994",
   },
-  robots: {
-    index: false,
-    follow: true,
-    nocache: true,
-    googleBot: {
-      index: true,
-      follow: false,
-      noimageindex: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
-  },
+  // robots: {
+  //   index: false,
+  //   follow: true,
+  //   nocache: true,
+  //   googleBot: {
+  //     index: true,
+  //     follow: false,
+  //     noimageindex: true,
+  //     "max-video-preview": -1,
+  //     "max-image-preview": "large",
+  //     "max-snippet": -1,
+  //   },
+  // },
 };
 
 export default function RootLayout({ children }) {
